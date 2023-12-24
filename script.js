54b84c88bfc706eda125cfe086551ba12f5f3f3a
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-24 21:23:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+  const colorBoard = document.getElementById('color-board');
+
+  function createColorBoxes() {
+    for (let i = 0; i < 800; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      colorBoard.appendChild(square);
+
+      // Add an event listener for each square
+      square.addEventListener('mouseover', function () {
+        // Change the color on hover
+        this.style.backgroundColor = 'coral';
+      });
+
+      square.addEventListener('mouseout', function () {
+        // Change the color back after 1 second
+        setTimeout(() => {
+          this.style.backgroundColor = 'lightgray';
+        }, 1000);
+      });
+    }
+  }
+
+  createColorBoxes();
+});
+
+
